304947fcc10f1d60033ef8e71c6228aed8829589
hebangwen <bangwhe@qq.com>
2021-08-13 10:33:32 +0800

    update hourglass full model

--- lib/models/hourglass.py
+++ lib/models/hourglass.py
@@ -2,6 +2,8 @@ import paddle
 import paddle.nn as nn
 import paddle.nn.functional as F
 
+import copy
+
 
 class ResidualModule(nn.Layer):
     def __init__(self, in_channels=256):
@@ -21,8 +23,7 @@ class ResidualModule(nn.Layer):
 
 class HourglassModule(nn.Layer):
     def __init__(self, heatmap_size=64, in_channels=256):
-        # hourglass模块的shape保证为(N, 64, 64, 256)
-        # TODO: 这个操作挺奇葩
+        # hourglass模块的shape保证为(N, 256, 64, 64)
         super().__init__()
         self.downsamples = nn.LayerList(
             [ResidualModule() for _ in range(4)]
@@ -56,11 +57,64 @@ class HourglassModule(nn.Layer):
         return x
 
 
+class Hourglass(nn.Layer):
+    def __init__(self, num_modules=4, img_size=256, feat_size=64, num_channels=256, in_channels=3):
+        super().__init__()
+
+        # 堆叠的hourglass模块的数量
+        self.num_modules = num_modules
+        # 输入网络的图像尺寸
+        self.img_size = img_size
+        # 输入hourglass模块的图像尺寸
+        self.feat_size = feat_size
+        # 输入hourglass模块的通道数
+        self.num_channels = num_channels
+        # 图片的通道数
+        self.in_channels = in_channels
+
+        # 对256的图像进行降采样
+        self.down_sample = nn.Sequential(
+            nn.Conv2D(kernel_size=7, stride=2, padding=3, in_channels=in_channels, out_channels=num_channels),
+            ResidualModule(in_channels=num_channels),
+            nn.MaxPool2D(kernel_size=2, stride=2)
+        )
+
+        self.hourglass_modules = nn.LayerList()
+        for i in range(self.num_modules):
+            module = nn.Sequential(
+                ('hourglass_module', HourglassModule()),
+                ('remap_3', nn.Conv2D(kernel_size=1, in_channels=self.num_channels, out_channels=self.in_channels)),
+                ('remap_256_1', nn.Conv2D(kernel_size=1, in_channels=self.in_channels, out_channels=self.num_channels)),
+                ('remap_256_2', nn.Conv2D(kernel_size=1, in_channels=self.in_channels, out_channels=self.num_channels))
+            )
+
+            self.hourglass_modules.append(module)
+
+    def forward(self, x):
+        x = self.down_sample(x)
+        output = []
+
+        for idx, m in enumerate(self.hourglass_modules):
+            _hg = m['hourglass_module']
+            c1 = m['remap_3']
+            c2 = m['remap_256_1']
+            c3 = m['remap_256_2']
+            y = _hg(x)
+            y = c1(y)
+            output.append(y)
+
+            y1 = c2(y)
+            y2 = c3(y)
+
+            x = x + y1 + y2
+
+        return output
+
+
 if __name__ == '__main__':
-    hg = HourglassModule()
+    hg = Hourglass()
     print(hg)
 
-    x = paddle.randn((4, 256, 64, 64))
+    x = paddle.randn((4, 3, 256, 256))
     y = hg(x)
-    print(y.shape)
-    print(y[0, 0])
+    print(y[0].shape, y[1].shape)
